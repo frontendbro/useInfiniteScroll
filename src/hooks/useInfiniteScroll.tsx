becf1b7b4bb00800efef7e7d92f5cdaeb900b0bc
Semyon Zadorozhniy <semyon.z@icloud.com>
2025-01-12 18:26:50 +0300

    docs

--- src/hooks/useInfiniteScroll.tsx
+++ src/hooks/useInfiniteScroll.tsx
@@ -7,10 +7,32 @@ type UseInfiniteScrollProps = {
   rootMargin?: string;
 }
 
+/**
+ * Custom hook for implementing infinite scroll functionality.
+ * @param {UseInfiniteScrollProps} props - The properties for the hook.
+ * @param {() => Promise<void>} props.fetchData - Function to fetch more data.
+ * @param {boolean} props.hasMore - Flag indicating if there is more data to load.
+ * @param {string} props.scrollTrigger - CSS selector for the scroll trigger element.
+ * @param {string} [props.rootMargin='200px'] - Margin around the root element for the intersection observer.
+ * @returns {[boolean]} - Returns an array with a single boolean indicating if data is being fetched.
+ */
 const useInfiniteScroll = ({ fetchData, hasMore, scrollTrigger, rootMargin = '200px' }: UseInfiniteScrollProps): [boolean] => {
+  /**
+   * State to track if data is being fetched.
+   * @type {boolean}
+   */
   const [isFetching, setIsFetching] = useState<boolean>(false);
+
+  /**
+   * Ref to store the IntersectionObserver instance.
+   * @type {React.MutableRefObject<IntersectionObserver | null>}
+   */
   const observer = useRef<IntersectionObserver | null>(null);
 
+  /**
+   * Callback function to fetch more data.
+   * @type {() => Promise<void>}
+   */
   const fetchMore = useCallback(async () => {
     await fetchData();
     setIsFetching(false);
