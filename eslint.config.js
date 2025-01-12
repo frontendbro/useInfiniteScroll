c81cec34f257cb7938f3b38cc062b3cab2506f2f
Semyon Zadorozhniy <semyon.z@icloud.com>
2025-01-12 15:35:16 +0300

    fix config

--- eslint.config.js
+++ eslint.config.js
@@ -23,26 +23,6 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
-
-        /** Stylistic */
-      "@stylistic/indent": ["warn", 2],
-      "@stylistic/quotes": ["warn", "single"],
-      "@stylistic/semi": ["warn", "never"],
-      "@stylistic/object-curly-spacing": ["warn", "always"],
-      "@stylistic/no-multi-spaces": ["warn"],
-      "@stylistic/no-multiple-empty-lines": ["warn", { "max": 1 }],
-      "@stylistic/eol-last": ["warn", "never"],
-
-      /** React */
-      "react/jsx-wrap-multilines": ["warn", { "return": "never", "arrow": "never" }],
-      "react/jsx-no-useless-fragment": ["warn"],
-      "react/jsx-one-expression-per-line": ["warn"],
-      "react/self-closing-comp": ["warn", { "component": true, "html": true }],
-      "react/jsx-first-prop-new-line": ["warn", "multiprop"],
-      "react/jsx-max-props-per-line": ["warn", { "maximum": 1 }],
-      "react/jsx-boolean-value": ["warn", "always"],
-      "react/jsx-tag-spacing": ["warn", { "beforeSelfClosing": "never" }],
-      "react/jsx-closing-bracket-location": ["warn", "line-aligned"]
     },
 
 
